055572ee8b2fcbcbe8ef336ba7afa2aeaf3b2cad
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2022-05-10 23:37:13 -0300

    Service layer, component registration

---
+++ src/main/java/com/udemy/udemy/services/UserService.java
@@ -0,0 +1,25 @@
+package com.udemy.udemy.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.udemy.entities.User;
+import com.udemy.udemy.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
