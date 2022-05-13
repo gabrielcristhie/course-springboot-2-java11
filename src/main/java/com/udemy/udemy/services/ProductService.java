6c25c316381dc83cd7bcd5ec4b869db565502ef0
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2022-05-13 00:16:03 -0300

    Product

---
+++ src/main/java/com/udemy/udemy/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.udemy.udemy.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.udemy.entities.Product;
+import com.udemy.udemy.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
