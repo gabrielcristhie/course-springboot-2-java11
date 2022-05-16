b084d55f55e969f83efaf41b59b9dfe5efbcc0c2
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2022-05-16 16:47:13 -0300

    Exception handling - delete

--- src/main/java/com/udemy/udemy/services/UserService.java
+++ src/main/java/com/udemy/udemy/services/UserService.java
@@ -3,8 +3,11 @@ package com.udemy.udemy.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.udemy.udemy.services.exceptions.DatabaseException;
 import com.udemy.udemy.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.udemy.entities.User;
@@ -12,6 +15,7 @@ import com.udemy.udemy.repositories.UserRepository;
 
 @Service
 public class UserService {
+
 	@Autowired
 	private UserRepository repository;
 	
@@ -27,7 +31,13 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
