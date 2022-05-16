e3b5d3481d13f739a8ee3247288ae2a60a96a131
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2022-05-16 16:41:37 -0300

    Exception handling - findById

--- src/main/java/com/udemy/udemy/services/UserService.java
+++ src/main/java/com/udemy/udemy/services/UserService.java
@@ -3,6 +3,7 @@ package com.udemy.udemy.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.udemy.udemy.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id){
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	public User insert(User obj) {
 		return repository.save(obj);
